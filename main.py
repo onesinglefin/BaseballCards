1e9707c246cda80f6e37c01bb833ee4d6cbf87d7
onesinglefin <33071759+onesinglefin@users.noreply.github.com>
2024-11-29 18:52:40 +0000

    1

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import streamlit as st
-# import sqlitecloud
+import sqlitecloud
 
 st.title('The Baseball App')
 
 st.image("https://en.wikipedia.org/wiki/Baseball_%28ball%29#/media/File:Baseball_(crop).jpg")
 
-# apikey= st.secrets["APIKEY"]
+apikey= st.secrets.APIKEY
 print(st.write(st.secrets["APIKEY"]))
-# conn = sqlitecloud.connect(f"sqlitecloud://cgxs5yl7hk.sqlite.cloud:8860?apikey={apikey}")
+conn = sqlitecloud.connect(f"sqlitecloud://cgxs5yl7hk.sqlite.cloud:8860?apikey={apikey}")
 
 # cursor = conn.execute("SELECT * FROM albums WHERE AlbumId = ?", (1, ))
 # result = cursor.fetchone()
