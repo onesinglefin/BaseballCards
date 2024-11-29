24df2d563df085135c0f69f16f7107abdb4ad837
onesinglefin <33071759+onesinglefin@users.noreply.github.com>
2024-11-29 18:41:57 +0000

    1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.title('The Baseball App')
 
 st.image("https://en.wikipedia.org/wiki/Baseball_%28ball%29#/media/File:Baseball_(crop).jpg")
 
-apikey= st.secrets("apikey")
+apikey= st.secrets("APIKEY")
 conn = sqlitecloud.connect(f"sqlitecloud://cgxs5yl7hk.sqlite.cloud:8860?apikey={apikey}")
 
 cursor = conn.execute("SELECT * FROM albums WHERE AlbumId = ?", (1, ))
