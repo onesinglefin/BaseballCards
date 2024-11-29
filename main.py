7474dc43fba5dfd144ead79ea8571b346ede8fc5
onesinglefin <33071759+onesinglefin@users.noreply.github.com>
2024-11-29 18:48:31 +0000

    1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.title('The Baseball App')
 
 st.image("https://en.wikipedia.org/wiki/Baseball_%28ball%29#/media/File:Baseball_(crop).jpg")
 
-apikey= st.secrets["APIKEY"]
+# apikey= st.secrets["APIKEY"]
 print(st.secrets)
 # conn = sqlitecloud.connect(f"sqlitecloud://cgxs5yl7hk.sqlite.cloud:8860?apikey={apikey}")
 
