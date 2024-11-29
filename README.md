a44654e616ce547a6c6fee12cc4be3a5b22b0e79
onesinglefin <33071759+onesinglefin@users.noreply.github.com>
2024-11-29 20:53:02 +0000

    1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # BaseballCards
-A basic baseball card app made by JD and his dad. We're learning about applications and databases!
+A basic **baseball card** app made by JD and his dad. We're learning about applications and databases! The app is made in `Python` using `Streamlit` and `SQLite Cloud`
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import streamlit as st
 import sqlitecloud
+import pandas as pd
 
 st.title('The Baseball App')
 
 apikey= st.secrets.APIKEY
 conn = sqlitecloud.connect(f"sqlitecloud://cgxs5yl7hk.sqlite.cloud:8860?apikey={apikey}")
 
-cursor = conn.execute("USE DATABASE chinook.sqlite; SELECT * FROM employees")
+cursor = conn.execute("USE DATABASE chinook.sqlite")
+
 result = cursor.fetchall()
 
-st.write(result)
-st.write("something should appear above this")
-# print(result)
+albums = pd.read_sql("SELECT * FROM albums LIMIT 20", conn)
+
+st.dataframe(albums, hide_index=True)
 
 conn.close()
