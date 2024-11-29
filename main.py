31ed0ce54f8331c43d99c0e609aaa024f593ed5b
onesinglefin <33071759+onesinglefin@users.noreply.github.com>
2024-11-29 16:43:36 +0000

    1

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import sqlitecloud
 
 st.title('The Baseball App')
 
 st.image("https://en.wikipedia.org/wiki/Baseball_%28ball%29#/media/File:Baseball_(crop).jpg")
+
+conn = sqlitecloud.connect("sqlitecloud://cgxs5yl7hk.sqlite.cloud:<your-host-port>?apikey=<your-api-key>")
+
+cursor = conn.execute("SELECT * FROM albums WHERE AlbumId = ?", (1, ))
+result = cursor.fetchone()
+
+print(result)
+
+conn.close()
