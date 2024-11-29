53d89af3fab34514824762f58da173450f0deb56
onesinglefin <33071759+onesinglefin@users.noreply.github.com>
2024-11-29 19:01:37 +0000

    1

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ apikey= st.secrets.APIKEY
 conn = sqlitecloud.connect(f"sqlitecloud://cgxs5yl7hk.sqlite.cloud:8860?apikey={apikey}")
 
 cursor = conn.execute("USE DATABASE chinook.sqlite; SELECT * FROM employees")
-result = cursor.fetchone()
+result = cursor.fetchall()
 
 st.write(result)
+st.write("something should appear above this")
 # print(result)
 
 conn.close()
