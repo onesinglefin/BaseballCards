89b1056bc44c2f62cb9edaded68f173b9e7095b0
onesinglefin <33071759+onesinglefin@users.noreply.github.com>
2024-12-09 15:52:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,9 @@ with st.form("card_input"):
   manufacturer = st.selectbox("Manufacturer",("Topps", "Upper Deck"))
   year = st.number_input("Insert a number", value=2024, min_value=1900, max_value=2025, step=1)
   sport = st.selectbox("Sport",("Baseball","Hockey","Football","Basketball"))
-  card_no = st.text_input("Card Number","001")
+  card_number = st.text_input("Card Number","001")
   owner = st. text_input("Owner","Suzie Who")
   submitted = st.form_submit_button("Submit")
   if submitted:
     st.write("Thank you for your submission")
+    cursor.execute('INSERT INTO card_list (player_name, manufacturer, year, sport, card_number, owner) VALUES (?, ?, ?, ?, ?, ?)', (player_name, manufacturer, year, sport, card_number, owner))
